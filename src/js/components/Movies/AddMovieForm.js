6541c6084e016a64874d9354d2fe4972e36aafe4
vickler charles <charlesvikler@gmail.com>
2019-11-30 22:06:56 -0500

    added validations to form

--- src/js/components/Movies/AddMovieForm.js
+++ src/js/components/Movies/AddMovieForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "../../../assets/sass/moviesList.sass";
-import { TextField, Grid, Button } from "@material-ui/core";
+import { TextField, Grid, Button, Typography } from "@material-ui/core";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ImageUpload from "../ImageUploader/ImageUpload";
@@ -14,11 +14,12 @@ class AddMovieForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
+      title: "",
       description: "",
       relaseDate: "",
       image: "",
-      react: true
+      react: true,
+      errors: {}
     };
   }
 
@@ -38,6 +39,29 @@ class AddMovieForm extends React.Component {
     });
   };
 
+  validate(values) {
+    console.log(values);
+    const formErrors = {};
+    const { title, description, image, relaseDate } = values;
+    if (title === "") {
+      formErrors.title = "this field is required";
+    }
+
+    if (description === "") {
+      formErrors.description = "this field is required";
+    }
+
+    if (image === "") {
+      formErrors.image = "please upload a new image";
+    }
+
+    if (relaseDate === "Invalid date") {
+      formErrors.relaseDate = "select a date";
+    }
+
+    return formErrors;
+  }
+
   setImage(image) {
     this.setState({
       ...this.state,
@@ -47,15 +71,28 @@ class AddMovieForm extends React.Component {
 
   onSaveMovie = () => {
     const movieToAdd = {
-      id: '_' + Math.random().toString(36).substr(2, 9),
+      id:
+        "_" +
+        Math.random()
+          .toString(36)
+          .substr(2, 9),
       title: this.state.title,
       description: this.state.description,
       relaseDate: moment(this.state.relaseDate.toString()).format("MMM Do YY"),
       image: this.state.image
     };
-    this.props.addMovie(movieToAdd);
-    this.setState({
-      redirect: true
+
+    const formErrors = this.validate(movieToAdd);
+    console.log(formErrors);
+    this.setState({ errors: formErrors }, () => {
+      const errorsArray = Object.values(this.state.errors);
+      const isError = errorsArray.some(value => value);
+      if (!isError) {
+        this.props.addMovie(movieToAdd);
+        this.setState({
+          redirect: true
+        });
+      }
     });
   };
 
@@ -72,6 +109,8 @@ class AddMovieForm extends React.Component {
                 onChange={this.handleChange}
                 value={this.state.title}
                 label="Title"
+                error={!!this.state.errors.title}
+                helperText={this.state.errors.title}
                 variant="filled"
               />
             </Grid>
@@ -82,6 +121,8 @@ class AddMovieForm extends React.Component {
                 onChange={this.handleChange}
                 name="description"
                 value={this.state.description}
+                error={!!this.state.errors.description}
+                helperText={this.state.errors.description}
                 multiline
                 label="Description"
                 variant="filled"
@@ -92,12 +133,37 @@ class AddMovieForm extends React.Component {
                 selected={this.state.relaseDate}
                 className="custom-date-picker"
                 showPopperArrow={false}
+                maxDate={moment().toDate()}
                 onChange={this.handleChangeDate}
                 placeholderText="Relase date"
               />
+              <br />
+              {this.state.errors.relaseDate && (
+                <Typography
+                  style={{
+                    color: "#f44336",
+                    fontSize: "0.75rem",
+                    marginLeft: "16px"
+                  }}
+                >
+                  {this.state.errors.relaseDate}
+                </Typography>
+              )}
             </Grid>
             <Grid item md={12}>
               <ImageUpload image={image => this.setImage(image)} />
+              <br />
+              {this.state.errors.image && (
+                <Typography
+                  style={{
+                    color: "#f44336",
+                    fontSize: "0.75rem",
+                    marginLeft: "16px"
+                  }}
+                >
+                  {this.state.errors.image}
+                </Typography>
+              )}
             </Grid>
             <Grid item md={12} style={{ textAlign: "right" }}>
               <Button
